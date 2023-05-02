3780d93eadc21dcf155f274a820d3f660a8f570a
ryanjhlee87 <108978592+ryanjhlee87@users.noreply.github.com>
2023-05-02 13:24:23 +0900

    Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const getVideos = async () => {
+    const videos = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos
+    &key=AIzaSyBSfmJfRWOmAyKNwK5ufS-CO8K4vSC8RUk`,
+      {},
+      { withCredentials: true }
+    );
+
+    if (videos) {
+      console.log(videos);
+    }
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl font-bold underline">
+      <h1>Hello Youtube</h1>
     </div>
   );
-}
+};
 
 export default App;
